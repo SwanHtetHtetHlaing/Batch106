a1e445108bdb282ce2bd32a3aeafa9b0827ced3d
User <User@SHHH-PC>
2024-03-13 13:27:45 +0630

    first commit

---
+++ src/array/Array_Example2.java
@@ -0,0 +1,19 @@
+package array;
+
+public class Array_Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a= {10,20,30,40,50};
+		for(int i=0; i<5;i++)
+		{
+			System.out.println(a[i]);
+		}
+//		System.out.println(a[0]);
+//		System.out.println(a[1]);
+//		System.out.println(a[2]);
+//		System.out.println(a[3]);
+//		System.out.println(a[4]);
+	}
+
+}
